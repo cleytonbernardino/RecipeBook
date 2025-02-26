b632eb015c04b26195792f13a30688be6455aeb4
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-02-26 16:04:11 -0300

    feat: adding user entity

---
+++ src/Backend/RecipeBook.Domain/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeBook.Domain.Entities
+{
+    public class EntityBase
+    {
+        public long ID { get; set; }
+        public bool Active { get; set; } = true;
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+}

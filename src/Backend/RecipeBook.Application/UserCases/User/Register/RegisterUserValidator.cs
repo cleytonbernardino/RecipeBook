494a706a46b31bdce80c0f2e100d56a35fd0c83b
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-02-27 17:17:51 -0300

    refactor: Changed the email validator to return only 1 error

--- src/Backend/RecipeBook.Application/UserCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/RecipeBook.Application/UserCases/User/Register/RegisterUserValidator.cs
@@ -10,8 +10,11 @@ namespace RecipeBook.Application.UserCases.User.Register
         {
             RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
             RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.EMAIL_EMPTY);
-            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
             RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_LENGTH_INVALID);
+            When(user => string.IsNullOrEmpty(user.Email) == false, () =>
+            {
+                RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+            });
         }
     }
 }

cd158ee389709c3929f7b5448911a01ae86cf190
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-02-27 14:41:42 -0300

    feat: Initial setup of user tests

---
+++ tests/CommonTestUtilities/Requests/RequestUserJsonBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using RecipeBook.Communiction.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestUserJsonBuilder
+    {
+        public static RequestRegisterUserJson MakeRequest()
+        {
+            return new Faker<RequestRegisterUserJson>()
+                .RuleFor(user => user.Name, (f) => f.Person.FullName)
+                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name, null, "gmail"))
+                .RuleFor(user => user.Password, (f) => f.Internet.Password());
+        }
+    }
+}
---
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,21 @@
+﻿using CommonTestUtilities.Requests;
+using RecipeBook.Application.UserCases.User.Register;
+using RecipeBook.Communiction.Requests;
+
+namespace Validators.Test.User.Register
+{
+    public class RegisterUserValidatorTest : RequestUserJsonBuilder
+    {
+        [Fact]
+        public void Succes()
+        {
+            RegisterUserValidator validator = new();
+
+            RequestRegisterUserJson request = MakeRequest();
+
+            bool result = validator.Validate(request).IsValid;
+
+            Assert.True(result);
+        }
+    }
+}

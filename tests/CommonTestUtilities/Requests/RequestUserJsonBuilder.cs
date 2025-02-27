789206584e583acdd31cc0df7095309d6af7e42c
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-02-27 17:36:43 -0300

    feat: unit tests for the validator

--- tests/CommonTestUtilities/Requests/RequestUserJsonBuilder.cs
+++ tests/CommonTestUtilities/Requests/RequestUserJsonBuilder.cs
@@ -5,12 +5,13 @@ namespace CommonTestUtilities.Requests
 {
     public class RequestUserJsonBuilder
     {
-        public static RequestRegisterUserJson MakeRequest()
+        public static RequestRegisterUserJson MakeRequest(int passwordLenght = 10)
         {
+            string provider = "gmail.com";
             return new Faker<RequestRegisterUserJson>()
                 .RuleFor(user => user.Name, (f) => f.Person.FullName)
-                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name, null, "gmail"))
-                .RuleFor(user => user.Password, (f) => f.Internet.Password());
+                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name, null, provider))
+                .RuleFor(user => user.Password, (f) => f.Internet.Password(passwordLenght));
         }
     }
 }
--- tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -1,6 +1,7 @@
 ﻿using CommonTestUtilities.Requests;
 using RecipeBook.Application.UserCases.User.Register;
 using RecipeBook.Communiction.Requests;
+using RecipeBook.Exceptions;
 
 namespace Validators.Test.User.Register
 {
@@ -9,13 +10,73 @@ namespace Validators.Test.User.Register
         [Fact]
         public void Succes()
         {
-            RegisterUserValidator validator = new();
-
             RequestRegisterUserJson request = MakeRequest();
 
+            RegisterUserValidator validator = new();
             bool result = validator.Validate(request).IsValid;
 
             Assert.True(result);
+        }
+
+        [Fact]
+        public void Erro_Name_Empty()
+        {
+            RequestRegisterUserJson request = MakeRequest();
+            request.Name = "";
+
+            RegisterUserValidator validator = new();
+            var result = validator.Validate(request);
+
+            Assert.False(result.IsValid);
+            Assert.Single(result.Errors);
+            Assert.Equal(result.Errors[0].ErrorMessage, ResourceMessagesException.NAME_EMPTY);
+        }
+
+        [Fact]
+        public void Erro_Email_Empty()
+        {
+            RequestRegisterUserJson request = MakeRequest();
+            request.Email = "";
+
+            RegisterUserValidator validator = new();
+            var result = validator.Validate(request);
+
+            Assert.False(result.IsValid);
+            Assert.Single(result.Errors);
+            Assert.Equal(result.Errors[0].ErrorMessage, ResourceMessagesException.EMAIL_EMPTY);
+        }
+
+        [Fact]
+        public void Erro_Email_Invalid()
+        {
+            RequestRegisterUserJson request = MakeRequest();
+            request.Email = "email.com";
+
+            RegisterUserValidator validator = new();
+            var result = validator.Validate(request); ;
+
+            Assert.False(result.IsValid);
+            Assert.Single(result.Errors);
+            Assert.Equal(result.Errors[0].ErrorMessage, ResourceMessagesException.EMAIL_INVALID);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void Erro_Password_Lenght_Invalid(int passwordLenght)
+        {
+            RequestRegisterUserJson request = MakeRequest(passwordLenght);
+
+            RegisterUserValidator validator = new();
+            var result = validator.Validate(request);
+
+            Assert.False(result.IsValid);
+            Assert.Single(result.Errors);
+            Assert.Equal(result.Errors[0].ErrorMessage, ResourceMessagesException.PASSWORD_LENGTH_INVALID);
         }
     }
 }

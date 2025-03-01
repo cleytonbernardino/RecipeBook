0693d515a9b7a1fa3a74e910de2891e7584d30ba
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-02-28 23:57:09 -0300

    fix: Fixed bug that prevented the application from starting

--- src/Backend/RecipeBook.Infrastructure/Extensions/ConfigurationExtension.cs
+++ src/Backend/RecipeBook.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -6,7 +6,13 @@ namespace RecipeBook.Infrastructure.Extensions
     {
         public static string ConnectionString(this IConfiguration configuration) => configuration.GetConnectionString("Connection")!;
 
-        public static bool IsUnitTestEnviroment(this IConfiguration configuration) => bool.Parse(configuration.GetSection("InMemoryTest").Value!);
+        public static bool IsUnitTestEnviroment(this IConfiguration configuration)
+        {
+            string? inMemoryTest = configuration.GetSection("InMemoryTest").Value;
+            if (string.IsNullOrEmpty(inMemoryTest))
+                return false;
+            return true;
+        }
     }
 }
 

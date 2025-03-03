706383d4bcbf7f8f93d109f96ad3150a904c41a4
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-03-03 19:52:58 -0300

    feat: usecase tests for login.

---
+++ tests/CommonTestUtilities/Requests/RequestLoginJsonBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using RecipeBook.Communiction.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestLoginJsonBuilder
+    {
+        public static RequestLoginJson Build()
+        {
+            string provider = "gmail.com";
+            return new Faker<RequestLoginJson>()
+                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(null, null, provider))
+                .RuleFor(user => user.Password, (f) => f.Internet.Password());
+        }
+    }
+}

c045ec87a476ba4a5c28f79aace0afc4afe8d0a6
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-04-20 17:50:43 -0300

    feat By not passing any parameters to recipe/filter, all recipes are returned.

--- src/Backend/RecipeBook.Infrastructure/DataAccess/Repositories/RecipeRepository.cs
+++ src/Backend/RecipeBook.Infrastructure/DataAccess/Repositories/RecipeRepository.cs
@@ -15,25 +15,33 @@ namespace RecipeBook.Infrastructure.DataAccess.Repositories
 
         public async Task<IList<Recipe>> Filter(User user, FilterRecipesDto filters)
         {
-            IQueryable<Recipe> query = _dbContext
-                    .Recipes
-                    .Include(recipe => recipe.Ingredients)
-                    .AsNoTracking()
-                    .Where(recipe => recipe.UserId == user.ID && user.Active);
+            var query = _dbContext
+                           .Recipes
+                           .AsNoTracking()
+                           .Include(recipe => recipe.Ingredients)
+                           .Where(recipe => recipe.Active && recipe.UserId == user.ID);
 
             if (filters.Difficulties.Any())
+            {
                 query = query.Where(recipe => recipe.Difficulty.HasValue && filters.Difficulties.Contains(recipe.Difficulty.Value));
+            }
 
             if (filters.CookingTimes.Any())
+            {
                 query = query.Where(recipe => recipe.CookingTime.HasValue && filters.CookingTimes.Contains(recipe.CookingTime.Value));
+            }
 
             if (filters.DishTypes.Any())
+            {
                 query = query.Where(recipe => recipe.DishTypes.Any(dishType => filters.DishTypes.Contains(dishType.Type)));
+            }
 
-            if (!string.IsNullOrEmpty(filters.RecipeTitle_Ingredient))
-                query = query.Where(recipe => recipe.Title.Contains(filters.RecipeTitle_Ingredient)
-                            || recipe.Ingredients.Any(ingredient => ingredient.Name.Contains(filters.RecipeTitle_Ingredient))
-                        );
+            if (!string.IsNullOrWhiteSpace(filters.RecipeTitle_Ingredient))
+            {
+                query = query.Where(
+                    recipe => recipe.Title.Contains(filters.RecipeTitle_Ingredient)
+                    || recipe.Ingredients.Any(ingredient => ingredient.Name.Contains(filters.RecipeTitle_Ingredient)));
+            }
 
             return await query.ToListAsync();
         }

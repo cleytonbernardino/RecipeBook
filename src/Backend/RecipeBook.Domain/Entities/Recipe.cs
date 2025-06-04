b9b2548ceb3577fd0bc384cd27f3127cab148629
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-06-04 00:13:00 -0300

    feat: New migration and change of revenue entity, to allow image support.

--- src/Backend/RecipeBook.Domain/Entities/Recipe.cs
+++ src/Backend/RecipeBook.Domain/Entities/Recipe.cs
@@ -1,14 +1,16 @@
-﻿namespace RecipeBook.Domain.Entities
+﻿using RecipeBook.Domain.Enums;
+
+namespace RecipeBook.Domain.Entities;
+
+public class Recipe : EntityBase
 {
-    public class Recipe : EntityBase
-    {
-        public long UserId { get; set; }
-        public DateTime LastUpdate { get; set; } = DateTime.UtcNow;
-        public string Title { get; set; } = "";
-        public Enums.CookingTime? CookingTime { get; set; }
-        public Enums.Difficulty? Difficulty { get; set; }
-        public IList<Ingredient> Ingredients { get; set; } = [];
-        public IList<Instruction> Instructions { get; set; } = [];
-        public IList<DishType> DishTypes { get; set; } = [];
-    }
+    public long UserId { get; set; }
+    public DateTime LastUpdate { get; set; } = DateTime.UtcNow;
+    public string Title { get; set; } = "";
+    public CookingTime? CookingTime { get; set; }
+    public Difficulty? Difficulty { get; set; }
+    public IList<Ingredient> Ingredients { get; set; } = [];
+    public IList<Instruction> Instructions { get; set; } = [];
+    public string? ImageIndentifier { get; set; }
+    public IList<DishType> DishTypes { get; set; } = [];
 }

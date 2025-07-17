e645f57280247103ba80bbc7b911c5f7baec2b25
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-07-16 21:29:43 -0300

    feat: Return the image URLs, if any, in the endpoints that were missing them: filter and dashboard.

--- src/Backend/RecipeBook.Application/UserCases/Recipe/Filter/FilterRecipeUseCase.cs
+++ src/Backend/RecipeBook.Application/UserCases/Recipe/Filter/FilterRecipeUseCase.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using RecipeBook.Application.Extensions;
 using RecipeBook.Communication.Requests;
 using RecipeBook.Communication.Responses;
 using RecipeBook.Domain.Dtos;
 using RecipeBook.Domain.Repositories.Recipe;
 using RecipeBook.Domain.Services.LoggedUser;
+using RecipeBook.Domain.Services.Storage;
 using RecipeBook.Exceptions.ExceptionsBase;
 
 namespace RecipeBook.Application.UserCases.Recipe.Filter
@@ -13,21 +15,26 @@ namespace RecipeBook.Application.UserCases.Recipe.Filter
         private readonly ILoggedUser _loggedUser;
         private readonly IRecipeReadOnlyRepository _respository;
         private readonly IMapper _mapper;
+        private readonly IBlobStorageService _blobStorageService;
 
         public FilterRecipeUseCase(
-            ILoggedUser loggedUser, IRecipeReadOnlyRepository repository, IMapper mapper 
+            ILoggedUser loggedUser,
+            IRecipeReadOnlyRepository repository,
+            IMapper mapper,
+            IBlobStorageService blobStorageService
         )
         {
             _loggedUser = loggedUser;
             _respository = repository;
             _mapper = mapper;
+            _blobStorageService = blobStorageService;
         }
 
         public async Task<ResponsesRecipesJson> Execute(RequestFilterRecipeJson request)
         {
             Validate(request);
             
-            var user = await _loggedUser.User();
+            var loggedUser = await _loggedUser.User();
 
             FilterRecipesDto filters = new()
             {
@@ -37,11 +44,11 @@ namespace RecipeBook.Application.UserCases.Recipe.Filter
                 DishTypes = request.DishTypes.Distinct().Select(c => (Domain.Enums.DishType)c).ToList()
             };
 
-            var recipes  = await _respository.Filter(user, filters);
+            var recipes  = await _respository.Filter(loggedUser, filters);
 
             return new ResponsesRecipesJson()
             {
-                Recipes = _mapper.Map<List<ResponseShortRecipeJson>>(recipes)
+                Recipes = await recipes.MapToShortRecipeJson(loggedUser, _blobStorageService, _mapper)
             };
         }
 
